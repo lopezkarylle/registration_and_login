427e5adcd26eca0c4d669de56b6b259918995795
lopezkarylle <dr.lopezkarylle@gmail.com>
2022-11-12 11:42:10 +0800

    first commit

---
+++ login-form.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Login Form</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+<style>
+	.login-form {
+		width: 340px;
+		margin: 50px auto;
+	}
+    .login-form form {        
+    	margin-bottom: 15px;
+        background: #f7f7f7;
+        box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+        padding: 25px;
+    }
+    .login-form h2 {
+        color: #636363;
+        margin: 0 0 15px;
+		text-align: center;
+    }
+    .form-control, .btn {
+        min-height: 38px;
+        border-radius: 2px;
+    }
+	.input-group-addon .fa {
+        font-size: 18px;
+    }
+    .btn {        
+        font-size: 15px;
+        font-weight: bold;
+    }
+</style>
+</head>
+<body>
+<br><br><br><br><br><br><br>
+<div class="login-form">
+    <form action="/examples/actions/confirmation.php" method="post">
+        <h2 class="text-center">Log In</h2>   
+        <div class="form-group">
+        	<div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                <input type="text" class="form-control" placeholder="Username" required="required">
+            </div>
+        </div>
+		<div class="form-group">
+            <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+                <input type="password" class="form-control" placeholder="Password" required="required">
+            </div>
+        </div>        
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary btn-block">Log in</button>
+        </div>
+    </form>
+</div>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,23 @@
+<?php
+
+include "vendor/autoload.php";
+
+use App\AuthClient;
+
+function loginUser(){
+    $token = '8b5bcf450e791c8f738eee4aac4e5533adb1579157715b20f9d5f43c0e809a8453863ab9b849a6ef75a65fc07a841c2d5a9f1ea55d81150bc2481e719d89b15439906a4e80bfc886019584be3c6aa0c70fbd01a3be3f3f3e5235a8fa809f9eab7203fddf3fb239a663180f3d7ed0ecd2f8ae8ff3ebb0453436319bd4beb05cce';
+
+$client = new AuthClient([
+    'base_uri' => 'http://localhost:1337/api/'
+]);
+
+$identifier = $_POST['username'];
+$password = $_POST['password'];
+
+$result = $client->login();
+
+var_dump($result->getStatusCode(__________________________));
+var_dump($result->getReasonPhrase());
+var_dump($result->getProtocolVersion());
+var_dump($result->getBody()->getContents());
+}
